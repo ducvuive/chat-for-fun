36824c2f79b826bd5d2c989ec1ff149719941f96
duc20062001@gmail.com <19521381@gm.uit.edu.vn>
2023-01-11 00:44:59 +0700

    upload web chat-for-fun

---
+++ WebApplication1/Models/MessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class MessageViewModel
+    {
+        [Required]
+        public string Content { get; set; }
+        public string Timestamp { get; set; }
+        public string From { get; set; }
+        public string Username { get; set; }
+        [Required]
+        public string Room { get; set; }
+        public string Avatar { get; set; }
+        public bool HasMoreMessages { get; set; }
+    }
+}
---
+++ WebApplication1/Validation/UsernameValidator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Validation
+{
+    public class UsernameValidator<TUser> : IUserValidator<TUser>
+      where TUser : IdentityUser
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<TUser> manager,
+                                                  TUser user)
+        {
+            if (user.UserName == null)
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                var userExist = manager.FindByNameAsync(user.UserName);
+                if (userExist.Result == null)
+                {
+                    return Task.FromResult(IdentityResult.Success);
+                }
+            }
+
+            return Task.FromResult(
+                     IdentityResult.Failed(new IdentityError
+                     {
+                         Code = "400",
+                         Description = "UserName is not unique 123."
+                     }));
+        }
+    }
+}
